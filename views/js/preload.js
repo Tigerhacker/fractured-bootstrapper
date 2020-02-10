d3a71f74c72b5b018fe68b0ed9e23d47aa5d24fc
Andrew <andrew1029384756@hotmail.com>
2020-02-10 22:57:34 +1100

    Expose steam

--- views/js/preload.js
+++ views/js/preload.js
@@ -3,6 +3,7 @@ const installer = require('./installer.js');
 const launcher = require('./launcher.js');
 const helper = require('./helper.js');
 const settings = require('./settings.js');
+const steam = require('./find-steam.js')
 
 window.interop = {};
 window.interop.isInstalled = installer.isInstalled;
@@ -18,3 +19,5 @@ window.interop.settingDel = settings.sotreDel;
 window.interop.settingGetFolder = settings.sotreGetFolder;
 
 window.interop.isTokenValid = helper.isTokenValid;
+
+window.interop.steam = steam;
